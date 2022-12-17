28e220bfefd587e297af72879cd732a8e827679f
jose.guerrerovazquez <joseguerrerovazquez@bundeswehr.org>
2022-12-17 11:16:53 +0100

    call it a date

--- src/views/VerzehrZettel.vue
+++ src/views/VerzehrZettel.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="T&P" class="noPrint" src="../assets/logo.png" />
+  <div class="verzehrZettel">
+    <div class="header">
+      <img alt="T&P" class="noPrint" src="../assets/logo.png" />
+      <div>
+        Verzehrzettel für <input class="dateInput" type="text" :value="getDate()" />
+      </div>
+    </div>
     <VerzehrZettelForm msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
-<script lang="ts">
+<script>
 import { defineComponent } from "vue";
 import VerzehrZettelForm from "@/components/VerzehrZettelForm.vue"; // @ is an alias to /src
 
@@ -14,7 +19,50 @@ export default defineComponent({
   components: {
     VerzehrZettelForm,
   },
+  setup() {
+    const getDate = () => {
+
+      const currentDate = new Date();
+
+      if(currentDate.getHours() < 8) {
+        currentDate.setDate(currentDate.getDate() - 1);
+      }
+
+      const options = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      };
+
+      return currentDate.toLocaleString("de-DE", options);
+    }
+    return {getDate}
+  }
 });
 </script>
+
+<style scoped>
+  .verzehrZettel {
+    display: flex;
+    flex-direction: column;
+  }
+  .header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .dateInput {
+    min-width: 220px;
+  }
+
+
+@media print {
+  .dateInput {
+    border: none;
+    font-weight: bold;
+  }
+}
+</style>
 
 
